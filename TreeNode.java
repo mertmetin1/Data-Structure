81a7c48a93291086685f1cdb878550a8989efe95
Mert Metin Erdemli <98667673+mertmetin1@users.noreply.github.com>
2022-12-21 14:23:06 +0300

    Add files via upload

---
+++ TreeNode.java
@@ -0,0 +1,16 @@
+
+public class TreeNode {
+	
+	     String directory_name ;
+	     int directory_size;
+	     TreeNode firstChild;
+	     TreeNode nextSibling; 
+	     public TreeNode(String directory_name,int directory_size,TreeNode firstChild,TreeNode nextSibling) {
+	    	 this.directory_name=directory_name;
+	    	 this.directory_size=directory_size;
+	    	 this.firstChild=firstChild;
+	    	 this.nextSibling=nextSibling;
+	    	 
+	     }
+
+}
