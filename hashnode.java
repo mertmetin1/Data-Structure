9f185c91a35ff904c62dbe5e3c98e14c37728fd9
Mert Metin Erdemli <98667673+mertmetin1@users.noreply.github.com>
2022-12-21 14:19:08 +0300

    Add files via upload

---
+++ hashnode.java
@@ -0,0 +1,12 @@
+package amkpackage;
+
+public class hashnode {
+int data;
+hashnode next;
+int hashnodeHash;
+public hashnode(int hashnodeSHash, int data) {
+this.data=data;
+next=null;
+this.hashnodeHash=hashnodeSHash;
+}
+}
